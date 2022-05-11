c70cead90e0741a94aa37abd9020362ce6cefa96
Ꮴᴀᴍᴘɪʀᴇ <103425355+TG-V4MP1R3@users.noreply.github.com>
2022-05-11 17:32:30 +0530

    File-Store-Bot

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DKBOTx/DKBOTx-FileStoreBot
+  git clone https://github.com/TG-V4MP1R3/File-Store-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DKBOTx-FileStoreBot
+  git clone $UPSTREAM_REPO /File-Store-Bot
 fi
-cd /DKBOTx-FileStoreBot
+cd /File-Store-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
